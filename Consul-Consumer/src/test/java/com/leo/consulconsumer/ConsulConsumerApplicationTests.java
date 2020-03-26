902f2ed6973094e5a6a65e58155f7738e2a761d9
username <707409741@qq.com>
2020-03-26 13:44:04 +0800

    'consul注册中心搭建与使用'

---
+++ Consul-Consumer/src/test/java/com/leo/consulconsumer/ConsulConsumerApplicationTests.java
@@ -0,0 +1,15 @@
+package com.leo.consulconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsulConsumerApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
---
+++ Consul-Provider/src/test/java/com/leo/consulprovider/ConsulProviderApplicationTests.java
@@ -0,0 +1,15 @@
+package com.leo.consulprovider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsulProviderApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
