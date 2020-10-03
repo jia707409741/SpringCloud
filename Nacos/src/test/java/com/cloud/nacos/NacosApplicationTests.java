8969940f0579a709764f9742f6d29955309d865a
朝花不迟暮 <707409741@qq.com>
2020-10-03 10:15:55 +0800

    update md

---
+++ Nacos/src/test/java/com/cloud/nacos/NacosApplicationTests.java
@@ -0,0 +1,15 @@
+package com.cloud.nacos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NacosApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
